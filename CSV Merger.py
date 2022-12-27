04b9c15674b9f29f3745c5020e1171a1e1df1185
MarcelGaida <68329815+marcel-gaida@users.noreply.github.com>
2022-12-27 23:11:02 +0100

    Updated CSV Merger script
    The script checks necessary dependencies before running. 
    If pandas and openpyxl libraries are not locally installed it will install them before running the script - if they are already installed it will just run the script for the merger.

--- CSV Merger.py
+++ CSV Merger.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python
+import subprocess
+
+# Check if pandas is installed
+try:
+    import pandas as pd
+except ImportError:
+    # If not, install pandas using subprocess
+    subprocess.run(["pip", "install", "pandas"])
+    # Reimport pandas
+    import pandas as pd
+
+#Check if pandas is installed
+try:
+    import openpyxl
+except ImportError:
+    # If not, install pandas using subprocess
+    subprocess.run(["pip", "install", "openpyxl"])
+    # Reimport
+    import openpyxl
+
+
+# Run the rest of the script
 import os
 import glob
-import pandas as pd
+
 
 # Get the current working directory (i.e., the directory where the script is located)
 csv_dir = os.getcwd()
@@ -15,7 +37,6 @@ df_merged = pd.DataFrame()
 # Iterate through the CSV files and read them into a data frame
 for i, file in enumerate(csv_files):
     df = pd.read_csv(file, sep='\t', header=None, names=[f"m{i+1}"])
-    #df = df.rename(columns={c: f"m{i+1}_{c}" for c in df.columns})
     df_merged = pd.concat([df_merged, df], axis=1)
 
 # Write the merged data frame to an Excel file
