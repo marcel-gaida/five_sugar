7ac2a70a5050e747c25f89e17ecc2d2aa6517b20
MarcelGaida <68329815+marcel-gaida@users.noreply.github.com>
2023-01-06 21:06:32 +0100

    Add files via upload
    Added changed to include a failsafe = False statement to prevent it script from ending on corners.

--- mouse mover.py
+++ mouse mover.py
@@ -14,12 +14,27 @@ import time
 #disable the failsafe
 pyautogui.FAILSAFE = False
 
+# get the screen size
+screen_width, screen_height = pyautogui.size()
+
+# set the initial direction of the mouse movement
+dx, dy = 1, 1
 print ("Press CRTL+C to stop the mover from running.")
 try:
     while True:
-        # move the mouse to a random position
+        # get the current mouse position
         x, y = pyautogui.position()
-        pyautogui.moveTo(x + 10, y + 10, duration=0.5)
-        time.sleep(0.5)
+
+        # change the direction if the mouse cursor is at or beyond the screen border
+        if x + dx > screen_width or x + dx < 0:
+            dx *= -1
+        if y + dy > screen_height or y + dy < 0:
+            dy *= -1
+
+        # move the mouse in the new direction
+        pyautogui.moveTo(x + dx, y + dy, duration=0.4)
+        time.sleep(0.4)
 except KeyboardInterrupt:
     print("Exiting program")
+
+
